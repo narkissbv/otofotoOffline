f3e836b8f65b0513af63fcf3827e0abc91e2704e
Matan Narkiss <matan.narkiss@bluevine.com>
2017-04-03 23:18:39 +0300

    added list.sh support for Mac and Linux

---
+++ photos/list.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+i=0
+echo "var files= [" > data.js
+for filename in *; do
+	if [ $filename != "list.sh" ] && [ $filename != "data.js" ] && [ $filename != "list.bat" ]; then
+		if [ $i = 0 ]; then
+			echo "	\"$filename\""  >> data.js
+		else
+			echo ",	\"$filename\"" >> data.js
+		fi
+		i=1
+	fi
+done
+echo "];" >> data.js
+echo Created data.js
