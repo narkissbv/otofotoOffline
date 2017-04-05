6facce56ddb6a3cd0b116ca287a61e5380ba8a8e
narkissbv <matan@matannarkiss.com>
2017-04-05 12:44:03 +0200

    list.bat for windows machines now runs from root directory

---
+++ list.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
+
+SET flag=true
+echo var files = [ > data.js
+for %%f in (photos/*.jpg) do (
+	if !flag!==true (
+		echo "%%f" >> data.js
+	) ELSE (
+		echo ,"%%f" >> data.js
+	)
+	SET flag=false
+)
+echo ] >> data.js
+echo Created data.js
